32270beab942023359b9631aab93cc2597d2fc7c
Michelle B <yhzgirl@gmail.com>
2013-03-22 17:08:49 +0000

    fixed css bug

--- db/migrate/20130320164212_add_pdf_and_e_book_and_softcover_and_hardcover_to_book.rb
+++
@@ -1,8 +0,0 @@
-class AddPdfAndEBookAndSoftcoverAndHardcoverToBook < ActiveRecord::Migration
-  def change
-    add_column :books, :PDF, :boolean
-    add_column :books, :eBook, :boolean
-    add_column :books, :softcover, :boolean
-    add_column :books, :hardcover, :boolean
-  end
-end
